d5bfb45f989d21e355b7e00ab7eae6542aa7c70c
Eduardaskuliesa <eduardas2000@mail.ru>
2023-02-20 01:02:33 +0200

    feat: Single Page View

--- Client/src/pages/solo-beef-card-page/solo-beef-card/index.tsx
+++ Client/src/pages/solo-beef-card-page/solo-beef-card/index.tsx
@@ -8,35 +8,88 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import Img from 'components/ui/img';
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  Rating,
+  Stack,
+} from '@mui/material';
 
 type SoloBeefProps = BeefModel;
 
 const SoloBeefCard: React.FC<SoloBeefProps> = ({
   price,
   cut,
-  img,
+  images,
   rating,
-  id,
   farm: {
     country,
-    city,
+    name,
   },
 }) => (
-  <Swiper
-    cssMode
-    navigation
-    pagination
-    mousewheel
-    keyboard
-    modules={[Navigation, Pagination, Mousewheel, Keyboard]}
-    className="mySwiper"
+  <Card sx={{
+    width: 500,
+    margin: 'auto',
+    height: 550,
+    boxShadow: 3,
+  }}
   >
-    <SwiperSlide>
-      <Img src={img[0]} />
-    </SwiperSlide>
-    <SwiperSlide><Img src={img[1]} /></SwiperSlide>
-    <SwiperSlide><Img src={img[2]} /></SwiperSlide>
-  </Swiper>
+    <CardMedia>
+      <Swiper
+        cssMode
+        navigation
+        pagination
+        mousewheel
+        keyboard
+        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <Img
+            sx={{ aspectRatio: '1.42', width: 1 }}
+            src={images[0]}
+          />
+        </SwiperSlide>
+        <SwiperSlide><Img sx={{ aspectRatio: '1.42', width: 1 }} src={images[1]} /></SwiperSlide>
+        <SwiperSlide><Img sx={{ aspectRatio: '1.42', width: 1 }} src={images[2]} /></SwiperSlide>
+      </Swiper>
+
+    </CardMedia>
+    <CardContent>
+      <Typography gutterBottom variant="h5" component="div">
+        {cut}
+      </Typography>
+      <Stack>
+        <Typography>
+          This beef cut is from
+          {' '}
+          {country}
+          ,
+          {' '}
+          {name}
+        </Typography>
+      </Stack>
+      <Typography>Lorem ipsum dolor sit amet.</Typography>
+      <Typography>
+        Price is often:
+        {' '}
+        {price}
+      </Typography>
+      <Stack justifyContent="center">
+        <Typography>
+          Rating for this cut is
+          {' '}
+          {rating}
+          {' '}
+          stars
+          {' '}
+        </Typography>
+      </Stack>
+    </CardContent>
+  </Card>
+
 );
 
 export default SoloBeefCard;
